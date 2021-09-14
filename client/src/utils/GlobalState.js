d9f7ae1b897db5fc231bdbba712b1023273ea9c6
Easton Shaum <Eastonshaum@gmail.com>
2021-09-14 15:08:51 -0600

    finished global state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,21 @@
-import React, { createContext, userContext } from "react";
-import { useProductReducer } from "./reducers";
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers'
 
-const StoreContext = createContext()
+const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-        products: [],
-        categories: [],
-        currentCategory: '',
-    })
-    // use this to confirm it works!
-    console.log(state)
-    return <Provider value={[state, dispatch]} {...props} />
-}
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
+
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
 const useStoreContext = () => {
-    return userContext(StoreContext)
-}
+  return useContext(StoreContext);
+};
 
 export { StoreProvider, useStoreContext };
